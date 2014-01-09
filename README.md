f79c4ae3736a7dc151bd6951619dc2ef3b85c57f
Scott Padgett <scottpadgett@Scott-Padgetts-MacBook-Pro.local>
2014-01-09 02:11:02 -0500

    Introduced a gradient-based simulation of fluid attraction. Ignoring CSS files

--- README.md
+++ README.md
@@ -1,22 +1,38 @@
 cohesion
 ========
 
-An unscientific attempt at emulating molecule cohesion in a fluid
+An unscientific attempt at simulating molecule cohesion in fluid blob interaction
 
 
 Idea here is to kind of create a pixel manipulation in canvas that will sort of behave like a lava lamp.
 
 
 
-Code Structure 
+CODE STRUCTURE 
 
 is a bare-bones architecture to focus on canvas manipulations.
 
 
 
-Dependencies 
+DEPENDENCIES
 
 are not managed but inline and are:
 
 jquery
 modernizr (is just here for kicks really)
+
+
+I'm using LiveReload server to preprocess LESS, LiveReload chrome extension to reserve code changes
+
+
+
+
+
+NOTES:
+
+1-8-13
+Found a neat way to simulate cohesion without processing every pixel as a fluid molecule using gradients.
+Adopted the concept from this guy:
+http://www.somethinghitme.com/2012/06/06/2d-metaballs-with-canvas/
+
+looking at simulating lavalamp behavior now with this overlapping gradient effect, next iterations will dig in
--- js/cohesion.js
+++ js/cohesion.js
@@ -3,14 +3,92 @@ var Cohesion = (function () {
 	var animationStartTimer;
 
 	function setupCanvas() {
+
+		// behavior properties
+	    var overlapThreshold = 200;
+	    var lightColorStop = 'rgba(255,100,0,1)';
+	    var darkColorStop = 'rgba(255,100,0,0)';
+	    var timeOut = 40;
+
 		var animationCanvas = document.getElementById('backgroundAnimation');
-		var $animationCanvas = $(animationCanvas);
-		var context = animationCanvas.getContext('2d');
+		var animationContext = animationCanvas.getContext('2d');
 
-		var canvasWidth = $animationCanvas.width();
-		var canvasHeight = $animationCanvas.height();
+		console.log("canvas dimensions = "+animationCanvas.width+"x"+animationCanvas.height);
 
-		console.log("canvas dimensions = "+canvasWidth+"x"+canvasHeight);
+	    var tempAnimCanvas = document.createElement("canvas");
+	    var tempAnimContext = tempAnimCanvas.getContext("2d");
+	    tempAnimCanvas.width = animationCanvas.width = 1200;
+	    tempAnimCanvas.height = animationCanvas.height = 600;
+
+	    // initialize points array
+	    points = [];
+
+		for(var i = 0; i < 50; i++){
+		    var x = Math.random() * animationCanvas.width,
+		        y = Math.random() * animationCanvas.height,
+		        vx = ( Math.random() * 8 ) - 4,
+		        vy = ( Math.random() * 8 ) -4 ,
+		        size = Math.floor( Math.random() * 60 ) + 60;
+		    
+		    points.push({ x:x, y:y, vx:vx, vy:vy, size:size });
+		                       
+		};
+
+		function update(){
+		    var len = points.length;
+		    tempAnimContext.clearRect( 0, 0, animationCanvas.width, animationCanvas.height );
+		    // draw a background first
+		    tempAnimContext.beginPath();
+		    tempAnimContext.fillStyle = 'rgb(0,0,0)';
+		    tempAnimContext.rect( 0, 0, animationCanvas.width, animationCanvas.height );
+		    tempAnimContext.fill();
+
+		    while( len-- ){
+		        var point = points[ len ];
+		        point.x += point.vx;
+		        point.y += point.vy;
+		        
+		        if(point.x > animationCanvas.width + point.size){
+		            point.x = 0 - point.size;
+		        }
+		        if(point.x < 0 - point.size){
+		            point.x = animationCanvas.width + point.size;
+		        }
+		        if(point.y > animationCanvas.height + point.size){
+		            point.y = 0 - point.size;
+		        }
+		        if(point.y < 0 - point.size){
+		            point.y = animationCanvas.height + point.size;
+		       }
+		        
+		        tempAnimContext.beginPath();
+		        var pointGradient = tempAnimContext.createRadialGradient( point.x, point.y, 1, point.x, point.y, point.size );
+		        pointGradient.addColorStop( 0, lightColorStop );
+		        pointGradient.addColorStop( 1, darkColorStop );
+		        tempAnimContext.fillStyle = pointGradient;
+		        tempAnimContext.arc( point.x, point.y, point.size, 0, Math.PI*2 );
+		        tempAnimContext.fill();
+		    }
+		    renderAgainstThreshold();
+		    setTimeout( update, timeOut );
+		}
+
+		function renderAgainstThreshold(){
+		    var imageData = tempAnimContext.getImageData( 0, 0, animationCanvas.width, animationCanvas.height ),
+		        pix = imageData.data;
+		    
+		    for ( var i = 0, n = pix.length; i <n; i += 4 ) {
+		        if( pix[ i + 20 ] < overlapThreshold ) {
+		            pix[ i + 20 ] /= 40;
+		            if( pix[ i + 20 ] > overlapThreshold / 27 ) {
+		                pix[ i + 20 ] = 0;
+		            }
+		        }
+		    }
+		    animationContext.putImageData(imageData, 0, 0);    
+		}
+
+		update(); 
 
 	}
 
