c999245b8a02931a73b6a9d903e4e2e54d05cf59
Scott Padgett <scottpadgett@Scott-Padgetts-MacBook-Pro.local>
2014-01-17 16:03:06 -0500

    Abstracted more behavior props and changed threshold filtering function.

--- js/cohesion.js
+++ js/cohesion.js
@@ -4,10 +4,15 @@ var Cohesion = (function () {
 
 	function setupCanvas() {
 
-		// behavior properties
-	    var overlapThreshold = 200;
-	    var lightColorStop = 'rgba(255,100,0,1)';
-	    var darkColorStop = 'rgba(255,100,0,0)';
+		// color/size behavior properties
+	    var overlapThreshold = 245;
+	    var lightColorStop = 'rgba(255,255,255,1)';
+	    var darkColorStop = 'rgba(240,240,240,0)';
+	    var backgroundColor = 'rgb(0,0,0)';
+	    var sizeBase = 200;
+	    var sizeMultiplier = 50;
+
+	    // motion behavior properties
 	    var timeOut = 10;
 
 		var animationCanvas = document.getElementById('backgroundAnimation');
@@ -17,8 +22,8 @@ var Cohesion = (function () {
 
 	    var tempAnimCanvas = document.createElement("canvas");
 	    var tempAnimContext = tempAnimCanvas.getContext("2d");
-	    tempAnimCanvas.width = animationCanvas.width = 1200;
-	    tempAnimCanvas.height = animationCanvas.height = 600;
+	    tempAnimCanvas.width = animationCanvas.width = 2400;
+	    tempAnimCanvas.height = animationCanvas.height = 1200;
 
 	    // initialize points array
 	    points = [];
@@ -26,9 +31,9 @@ var Cohesion = (function () {
 		for(var i = 0; i < 50; i++){
 		    var x = Math.random() * animationCanvas.width,
 		        y = Math.random() * animationCanvas.height,
-		        vx = ( Math.random() * 8 ) - 4,
-		        vy = ( Math.random() * 8 ) -4 ,
-		        size = Math.floor( Math.random() * 60 ) + 60;
+		        vx = ( Math.random() * 4 ) - 2,
+		        vy = ( Math.random() * 4 ) - 2 ,
+		        size = Math.floor( Math.random() * sizeMultiplier ) + sizeBase;
 		    
 		    points.push({ x:x, y:y, vx:vx, vy:vy, size:size });
 		                       
@@ -39,7 +44,7 @@ var Cohesion = (function () {
 		    tempAnimContext.clearRect( 0, 0, animationCanvas.width, animationCanvas.height );
 		    // draw a background first
 		    tempAnimContext.beginPath();
-		    tempAnimContext.fillStyle = 'rgb(0,0,0)';
+		    tempAnimContext.fillStyle = backgroundColor;
 		    tempAnimContext.rect( 0, 0, animationCanvas.width, animationCanvas.height );
 		    tempAnimContext.fill();
 
@@ -75,16 +80,23 @@ var Cohesion = (function () {
 
 		function renderAgainstThreshold(){
 		    var imageData = tempAnimContext.getImageData( 0, 0, animationCanvas.width, animationCanvas.height ),
-		        pix = imageData.data;
+		        pixels = imageData.data;
 		    
-		    for ( var i = 0, n = pix.length; i <n; i += 4 ) {
-		        if( pix[ i + 20 ] < overlapThreshold ) {
-		            pix[ i + 20 ] /= 40;
-		            if( pix[ i + 20 ] > overlapThreshold / 27 ) {
-		                pix[ i + 20 ] = 0;
+		    var pixelVal;
+		    for ( var i = 0, n = pixels.length; i < n; i += 4 ) {
+		    	pixelVal = pixels[i];
+		        if( pixelVal < overlapThreshold ) {
+		            pixelVal = 0;
+		            if( pixelVal >= overlapThreshold ) {
+		                pixelVal = 0;
 		            }
 		        }
+		        pixels[i] = pixelVal;
+		        pixels[i+1] = pixelVal;
+		        pixels[i+2] = pixelVal;
+
 		    }
+		    animationContext.clearRect( 0, 0, animationCanvas.width, animationCanvas.height );
 		    animationContext.putImageData(imageData, 0, 0);    
 		}
 
